c64392178af3beee678db8ae7c6ae09a8106beab
Priyanshu Parashar <priyanshuparashar08@gmail.com>
2025-08-31 03:52:40 +0530

    setup daisyui

---
+++ apps/web/src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <p>In app components!</p>
+      <button className="btn btn-outline btn-primary font-medium text-xl">
+        Holla!
+      </button>
+    </div>
+  );
+}
+
+export default App;
