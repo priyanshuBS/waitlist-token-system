0a00826fcc6c9df784d3bb694105429d5e8bcf58
Priyanshu Parashar <priyanshuparashar08@gmail.com>
2025-09-10 23:28:39 +0530

    setup basic

--- frontend/src/Routes.tsx
+++ frontend/src/Routes.tsx
@@ -2,6 +2,8 @@ import { createBrowserRouter } from "react-router-dom";
 import RootLayout from "./RootLayout";
 import Home from "./pages/Home";
 import LandingPage from "./pages/LandingPage";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
 
 const appRouter = createBrowserRouter([
   {
@@ -15,6 +17,14 @@ const appRouter = createBrowserRouter([
       {
         path: "home",
         element: <Home />,
+      },
+      {
+        path: "/login",
+        element: <Login />,
+      },
+      {
+        path: "/signup",
+        element: <Signup />,
       },
     ],
   },
---
+++ frontend/src/pages/Signup.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Signup: React.FC = () => {
+  return (
+    <div>
+      <p>In singup page!</p>
+    </div>
+  );
+};
+
+export default Signup;
