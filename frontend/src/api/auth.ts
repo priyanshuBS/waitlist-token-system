cbad2b4bd728524d35a0a5a2b2e8d78df46cfb23
Priyanshu Parashar <priyanshuparashar08@gmail.com>
2025-09-14 09:59:20 +0530

    remove backend & frontend

--- frontend/src/api/auth.ts
+++
@@ -1,8 +0,0 @@
-import api from "./api";
-
-export const signup = (data: {
-  fullName: string;
-  email?: string;
-  phoneNumber?: string;
-  password: string;
-}) => api.post("/auth/signup", data);
