13dba767f09f4e8939e6509ebaf6ddb5783b1b6e
Priyanshu Parashar <priyanshuparashar08@gmail.com>
2025-09-27 11:16:36 +0530

    setup cors & cookie-parser

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,17 @@
 import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
 const app = express();
+
+const corsOptions = {
+  origin: process.env.CLIENT_URL,
+  credentials: true,
+};
+
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(cookieParser());
+app.use(cors(corsOptions));
 
 export default app;
